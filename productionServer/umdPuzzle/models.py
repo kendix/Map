9706e78774f02078e26d2d21c1c7975e30ad9ca3
Eric Yang <eyang24@tterpmail.umd.edu>
2016-11-20 10:36:15 -0500

    redo

---
+++ productionServer/umdPuzzle/models.py
@@ -0,0 +1,84 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from math import sin, cos, sqrt, atan2, radians
+
+# Create your models here.
+
+@python_2_unicode_compatible
+class Place(models.Model):
+    """ 
+    This defines a puzzle's picture 
+    (located as a static file, stored as a string),
+    the puzzle's coordinates, whether the puzzle is completed, 
+    and everyone who has solved the puzzle 
+    """
+
+    name = models.CharField(max_length=20, unique=True,
+        help_text="name of the location/puzzle")
+    latitude = models.FloatField(null=False, blank=False,
+        help_text="upper most latitude")
+    longitude = models.FloatField(null=False, blank=False, 
+        help_text="left most longitude")
+    # only one puzzle can have the same picture url
+    picture = models.CharField(max_length=100, unique=True, 
+        help_text="name of picture") 
+
+    completed = models.BooleanField(default=False, 
+        help_text="True if puzzle is completed, false if incomplete")
+
+    # solvers will be stored as a string delimited by ||
+    # ex: 'user1'||'user2'||'user3'
+    solvers = models.TextField(null=True, blank=True, help_text="users who have solved the puzzle")
+
+    def __str__(self):
+        return self.name
+
+    def getDistance(self, latitude, longitude):
+        """ 
+        returns the distance between the place's location 
+        and a different longitude/latitude 
+        """
+        # Radius of Earth in Miles
+        
+        R = 3959.0
+        lat1 = radians(self.latitude)
+        lon1 = radians(self.longitude)
+        lat2 = radians(latitude)
+        lon2 = radians(longitude)
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = R * c
+
+        return distance
+
+    def addSolver(self, solver_name):
+        if self.solvers == "":
+            self.solvers = solver_name
+        else:
+            self.solvers = self.solvers + "||" + solver_name
+
+    def getSolvers(self):
+        if self.solvers == "":
+            return []
+        return self.solvers.split("||")
+
+    def getScore(self):
+        solvers = self.getSolvers()
+        if len(solvers) == 1:
+            return 500
+        elif len(solvers) == 2:
+            return 300
+        elif len(solvers) == 3:
+            return 100
+        else:
+            return 50
+
+
+
---
+++ productionServer/umdPuzzle/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+import json
+from models import Place
+
+# Create your views here.
+def index(request):
+    example = {
+        'solved' : 'false',
+        'users_completed' : [
+        ],
+    }
+    return HttpResponse(json.dumps(example), content_type='application/json')
+
+def getCompletedPuzzles(request):
+    place_qs = Place.objects.filter(completed=True)
+
+    response_data = {}
+    completedPuzzles = []
+    for place in place_qs:
+        temp_obj = {
+            'name' : place.name,
+            'id' :  place.id,
+        }
+        completedPuzzles.append(temp_obj)
+
+    response_data['completedPuzzles'] = completedPuzzles
+
+    return JsonResponse(response_data)
+
+def postPuzzleSolved(request):
+    response_data = {}
+    if request.method != "POST":
+        response_data['status'] = "failed"
+        response_data['score'] = 0
+        return JsonResponse(response_data)
+    else:
+        puzzle_id = int(request.POST['puzzle_id'])
+        username = request.POST['username']
+
+        get_place = get_object_or_404(Place, pk=puzzle_id)
+        get_place.addSolver(username)
+        get_place.completed = True
+        get_place.save()
+        score = get_place.getScore()
+
+
+        response_data['status'] = "success"
+        response_data['score'] = score
+
+        return JsonResponse(response_data)
+
+
+def getPlacePic(request, place_id):
+    get_place = get_object_or_404(Place, pk=place_id)
+    context = {
+        'picture' : "img/" + get_place.picture
+    }
+    return render(request, 'umdPuzzle/pic.html', context)
+
+
+def getPlace(request):
+    response_data = {}
+    if request.GET.get('lat') and request.GET.get('lon'):
+        # convert to float
+        fLat = float(request.GET.get('lat'))
+        fLon = float(request.GET.get('lon'))
+
+        places_qs = Place.objects.all()
+        closest_place = None
+        min_distance = None
+
+        for place in places_qs:
+            curr_dist = place.getDistance(fLat, fLon)
+        
+            if min_distance == None or curr_dist < min_distance:
+                min_distance = curr_dist
+                closest_place = place
+        if closest_place is not None :
+            response_data['distance'] = min_distance
+
+            # get the solvers of the place
+            solvers = closest_place.getSolvers()
+
+            response_data['place'] = {
+                'id' : closest_place.id,
+                'name' : closest_place.name,
+                'latitude' : closest_place.latitude,
+                'longitude' : closest_place.longitude,
+                'pic' : closest_place.picture,
+                'completed' : closest_place.completed,
+                'solvers' : solvers,
+            }
+        else:
+            response_data['distance'] = "no objects in database"
+            response_data['place'] = {}
+
+
+    else:
+        response_data['distance'] = "lat and lon parameters nonexistant"
+        response_data['place'] = {}
+
+    return JsonResponse(response_data)
+
