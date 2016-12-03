6f939f01a378bc77d017b01ec011594e804af985
Eric Yang <eyang24@tterpmail.umd.edu>
2016-12-03 15:45:04 -0500

    do something

--- PuzzleActivityProject/PuzzleActivityProject/app/src/main/java/com/example/daneil/puzzleactivityproject/PuzzleActivity.java
+++ PuzzleActivityProject/PuzzleActivityProject/app/src/main/java/com/example/daneil/puzzleactivityproject/PuzzleActivity.java
@@ -23,7 +23,7 @@ public class PuzzleActivity extends AppCompatActivity implements View.OnTouchLis
 
     float dX, dY;
     private float wiggle = .25f;
-    int[] pd = {2, 2}; // Puzzle Dimentions
+    int[] pd = {5, 3}; // Puzzle Dimentions
     ArrayList<PieceView> pieces = new ArrayList<PieceView>();
 
     @Override
@@ -42,7 +42,7 @@ public class PuzzleActivity extends AppCompatActivity implements View.OnTouchLis
                 int x = width / pd[0];
                 int y = height / pd[1];
 
-                fullImage = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
+                fullImage = BitmapFactory.decodeResource(getResources(), R.drawable.test4);
                 fullImage = Bitmap.createScaledBitmap(fullImage, width, height, false);
 
                 for (int i = 0; i < pd[0]; i++) {
