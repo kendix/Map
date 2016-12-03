1015a228f0e0d18ee3b1fa7022ca2dfac3b66fbb
Eric Yang <eyang24@tterpmail.umd.edu>
2016-12-03 15:51:00 -0500

    added stuff about multiplayer maps

---
+++ Erics Stuff/MapsActivity.java
@@ -0,0 +1,232 @@
+package com.example.eggrolleric.myapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        GoogleMap.OnMarkerClickListener {
+
+    private static final int REQ_ACCESS_FINE_LOCATION = 1337;
+    private static final int PLACE_DESCRIPTION_ACTIVITY = 2;
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LatLng mLatLng;
+    private Marker currLocation;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+    private HashMap<String, Object> mPlacesMap;
+    protected ArrayList<Marker> markerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
+
+        // Set Current Location, asks user for permissions,
+        // finishes activity if permissions are not given
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            buildGoogleApiClient();
+        } else {
+            Toast.makeText(MapsActivity.this,
+                    "Please Give Permissions", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQ_ACCESS_FINE_LOCATION);
+        }
+        // Here execute asynchtask to get all buildings
+        // onPostExecute for asynchtask should create all the markers needed
+        //      Red for incomplete, Green for Complete
+        // Markers should
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQ_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                        buildGoogleApiClient();
+                    }
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MapsActivity.this,
+                            "Permissions Not Sufficient to Start Activity",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+
+                }
+                return;
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Toast.makeText(this,"Building API Client, REMOVE FOR FINAl",Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this,"onConnected",Toast.LENGTH_SHORT).show();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+        }
+        if (mLastLocation != null) {
+            //place marker at current position
+            //mGoogleMap.clear();
+            mLatLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(mLatLng);
+            markerOptions.title("Current Position");
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+            currLocation = mMap.addMarker(markerOptions);
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); //5 seconds
+        mLocationRequest.setFastestInterval(3000); //3 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this,"Connection Suspended",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this,"Connection Failed",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        //place marker at current position
+        //mGoogleMap.clear();
+        if (currLocation != null) {
+            currLocation.remove();
+        }
+        mLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(mLatLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+        currLocation = mMap.addMarker(markerOptions);
+        Toast.makeText(this,"Location Changed",Toast.LENGTH_SHORT).show();
+
+        //zoom to current position:
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(mLatLng).zoom(14).build();
+
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        //If you only need one location, unregister the listener
+        //LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        // should start activity to show information about the place+
+        if (marker != currLocation) {
+            String placeName = marker.getTitle();
+            Intent startPlaceDescription = new Intent(this, PlaceDescription.class);
+            startPlaceDescription.putExtra("placeName", placeName);
+            startActivityForResult(startPlaceDescription, PLACE_DESCRIPTION_ACTIVITY);
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "This is your current location!", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    public void addPlace(Object place) {
+        mPlacesMap.put(place.toString(), place);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        updateMarkers();
+    }
+
+    private void updateMarkers() {
+        // Remove all markers, new ones will be added
+        for (Marker marker : markerList){
+            marker.remove();
+        }
+        // Todo -- use asynctask to update all the markers
+    }
+
+}
