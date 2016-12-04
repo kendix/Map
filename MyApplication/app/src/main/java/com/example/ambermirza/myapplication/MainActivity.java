4a101d274ce84e1a6aa1ddc0a6913909e109c8d3
Amber Mirza <ambermirza89@gmail.com>
2016-12-04 15:11:48 -0500

    Update MainActivity.java

--- MyApplication/app/src/main/java/com/example/ambermirza/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ambermirza/myapplication/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.ambermirza.myapplication;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.View;
-import android.widget.TextView;
-import android.app.ActionBar.LayoutParams;
+
 import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.view.View;
+import android.content.Intent;
 
 /**
  * Created by ambermirza on 12/3/16.
  */
 
 public class MainActivity extends Activity{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        TextView text = new TextView(this);
-        text.setText("Press the menu button to get list of menus.");
-        addContentView(text, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        setContentView(R.layout.activity_main);
 
     }
 
@@ -31,7 +30,11 @@ public class MainActivity extends Activity{
         inflater.inflate(R.menu.menu_myactivity, menu);
         return true;
     }
-
+    public void map(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+    //jk not using this anymore
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Toast.makeText(getApplicationContext(),	item.getTitle() + " selected", Toast.LENGTH_SHORT).show();
